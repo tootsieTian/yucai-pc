0c72fc819a9800d93a312f69027fa06b9b4fc6c3
谢海铭 <7936116+xie_haiming@user.noreply.gitee.com>
2021-05-31 15:41:32 +0800

    解决冲突

--- src/views/personal/index.vue
+++ src/views/personal/index.vue
@@ -1,7 +1,20 @@
 <template>
   <div  class="container-1430">
 	<div class="header" >
-		
+		<div class="user-info" >
+			<div class="info-left" >
+				<div class="user-pic" ></div>
+				<div class="user-detail" >
+					<div class="detail-name" >薛定谔的猫</div>
+					<div class="detail-id" >ID:922598</div>
+				</div>
+			</div>
+			<div class="info-left" >
+				<div v-for="(item,index) in subList" :key="index" >
+					
+				</div>
+			</div>
+		</div>
 	</div>
 	<div class="aside" >
 		<el-col :span="12">
@@ -10,7 +23,7 @@
 		      class="el-menu-vertical-demo"
 		      @open="handleOpen"
 		      @close="handleClose">
-		      <el-menu-item v-for="(item,index) in menuList" :index="index+1">
+		      <el-menu-item v-for="(item,index) in menuList" :key="index" :index="index+1">
 		        <i class="el-icon-menu"></i>
 		        <template #title>{{item}}</template>
 		      </el-menu-item>
@@ -36,7 +49,8 @@
 	    return {
 	      menuList:[
 			  "个人资料","我的消息","测评中心","我的积分","浏览记录","领赠记录","我的评价","申请服务商"
-		  ]
+		  ],
+		  subList:[{name:"课程收藏",num:22},{name:"我的订单",num:10},{name:"我的活动",num:2},{name:"优惠券",num:6},{name:"我的收益",num:"￥115"},]
 	    }
 		}
   }
